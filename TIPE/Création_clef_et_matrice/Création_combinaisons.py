d7e8d6ec0752b4428b7de4136b9d3e256a3d13f2
Pasquier Romain <78238743+romainpasquier@users.noreply.github.com>
2021-03-29 23:47:04 +0200

    Delete TIPE directory

--- TIPE/Création_clef_et_matrice/Création_combinaisons.py
+++
@@ -1,8 +0,0 @@
-def combinaisons(n):
-    L = []
-    for a in range(1,n+1):
-        for b in range(1, n+1):
-            for c in range(1, n+1):
-                for d in range(1, n+1):
-                    L.append([a, b, c, d])
-    return L
--- TIPE/Création_clef_et_matrice/Transpositon_clef.py
+++
@@ -1,5 +0,0 @@
-# Pour pouvoir bouger dans la clef, je mets ma clef sous forme de liste de longueur 24
-def transpo_clef(ecrit):
-    for i in range(len(ecrit)):
-        ecrit[i] = int(ecrit[i])
-    return ecrit
